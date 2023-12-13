8e3a7fb7f0eb1c7a31c73811a007d19df32f639e
PC-2023 <testalgjegwk984@gmail.com>
2023-12-13 21:05:52 +0800

    Early december update

--- hotel-booking/flutter_service_worker.js
+++ hotel-booking/flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
-"assets/NOTICES": "1c1af7a96477dd23b121bf6b33c55714",
+"assets/NOTICES": "8bdcef367acf2c4fc1d68b94273dc34d",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
 "canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e",
@@ -18,9 +18,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b801068111abd56a6961b013123e3bee",
-"/": "b801068111abd56a6961b013123e3bee",
-"main.dart.js": "57fe37c257c1afc02d25bc1039364189",
+"index.html": "d6543c78d2fba4d2f247c8ebfe8f087d",
+"/": "d6543c78d2fba4d2f247c8ebfe8f087d",
+"main.dart.js": "bdb1b852876abd606a4daebad4552e16",
 "manifest.json": "afb1ebf44a02ee2f7200eee926ddda6f",
 "version.json": "7c8a6143d435f17043545767bd3d5885"
 };
